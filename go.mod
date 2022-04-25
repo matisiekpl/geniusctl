09a097f57dee5f2e4a85bb82a4def0dffd0a4aff
Mateusz Woźniak <matisiek11@gmail.com>
2022-04-25 16:46:56 +0200

    :sparkles: Implemented basic functionality

---
+++ go.mod
@@ -0,0 +1,18 @@
+module genius-cli
+
+go 1.18
+
+require (
+	github.com/daskol/go.lyrics v0.0.0-20180928141243-b7299ce62709
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171
+)
+
+require (
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20220421235706-1d1ef9303861 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/antchfx/htmlquery"
+	"golang.org/x/term"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Cyan = "\033[36m"
+var White = "\033[97m"
+var DarkGray = "\u001b[38;5;238m"
+
+func printManyTimes(contents string, n int, color string) {
+	out := color
+	for i := 0; i < n; i++ {
+		out += contents
+	}
+	fmt.Print(out)
+}
+
+func printLyrics(header string, lyrics string) {
+	offset := 2
+	lines := strings.Split(lyrics, "\n")
+	width, _, _ := term.GetSize(0)
+
+	printManyTimes("─", offset, DarkGray)
+	fmt.Print("┬")
+	printManyTimes("─", width-offset-1, DarkGray)
+	fmt.Println()
+	printManyTimes(" ", offset, DarkGray)
+	fmt.Print("│")
+	fmt.Println(White + " " + header)
+	printManyTimes("─", offset, DarkGray)
+	fmt.Print("┼")
+	printManyTimes("─", width-offset-1, DarkGray)
+
+	for _, line := range lines {
+		printManyTimes(" ", offset, DarkGray)
+		fmt.Print("│")
+		if strings.Contains(line, "[") {
+			fmt.Println(" " + Cyan + line)
+		} else {
+			fmt.Println(" " + White + line)
+		}
+	}
+
+	printManyTimes("─", offset, DarkGray)
+	fmt.Print("┴")
+	printManyTimes("─", width-offset-1, DarkGray)
+	fmt.Print(Reset)
+}
+
+func printSong(song Song) {
+	resp, _ := http.Get(song.Url)
+	defer resp.Body.Close()
+	html, _ := ioutil.ReadAll(resp.Body)
+	doc, _ := htmlquery.Parse(strings.NewReader(strings.ReplaceAll(string(html), "<br/>", "\n")))
+	list := htmlquery.Find(doc, "//div[@data-lyrics-container]")
+	lyrics := ""
+	for _, el := range list {
+		lyrics += htmlquery.InnerText(el)
+	}
+	printLyrics(song.Title+" - "+song.ArtistNames, lyrics)
+}
+
+type Song struct {
+	Id          int    `json:"id"`
+	Path        string `json:"path"`
+	Url         string `json:"url"`
+	ArtistNames string `json:"artist_names"`
+	Title       string `json:"title"`
+}
+
+type Hit struct {
+	Index  string `json:"index"`
+	Type   string `json:"type"`
+	Result Song   `json:"result"`
+}
+
+type Section struct {
+	Type string `json:"type"`
+	Hits []Hit  `json:"hits"`
+}
+
+type SearchResponseResponse struct {
+	Sections []Section `json:"sections"`
+}
+
+type SearchResponse struct {
+	Response SearchResponseResponse `json:"response"`
+}
+
+func search(query string) Song {
+	resp, _ := http.Get("https://genius.com/api/search/multi?per_page=5&q=" + url.QueryEscape(query))
+	defer resp.Body.Close()
+	responseData, _ := ioutil.ReadAll(resp.Body)
+	var result SearchResponse
+	json.Unmarshal(responseData, &result)
+	return result.Response.Sections[1].Hits[0].Result
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(Red + "Song title not given")
+		os.Exit(1)
+	}
+	printSong(search(strings.Join(os.Args[1:], " ")))
+}
